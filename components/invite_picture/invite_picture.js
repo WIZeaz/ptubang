de9964e8ef15b7d0e0272f265f13b7229e654804
WIZeaz <764696642@qq.com>
2020-05-09 18:01:23 +0800

    merge branch 'yeyan'

---
+++ components/invite_picture/invite_picture.js
@@ -0,0 +1,26 @@
+// components/invite_picture.js
+Component({
+  options: {
+    "addGlobalClass": true,
+    "styleIsolation": "apply-shared"
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    "blockInfo": Object
+  },
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
